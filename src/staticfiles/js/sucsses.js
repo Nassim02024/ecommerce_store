92733fc5fe7800f8935c3effd7290fb6e564b67e
Nassim02024 <nn1509789@gmail.com>
2025-08-09 06:11:06 +0100

    all

--- src/staticfiles/js/sucsses.js
+++
@@ -1,8 +0,0 @@
-let sucssesBtn = document.querySelector('.sucsses-btn')
-sucssesBtn.addEventListener('click', () => {
-  localStorage.removeItem("arrayProduct")
-  localStorage.removeItem("arrayProductCount")
-  localStorage.removeItem("arrayProductPrice")
-  localStorage.removeItem("arrayProductImg")
-    
-})
