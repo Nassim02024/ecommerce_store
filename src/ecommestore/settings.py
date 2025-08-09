56bb9dcd0bf7a221f924ccceeaf27b04b5a8cfd9
Code_Line <nn1509789@gmail.com>
2025-08-09 06:21:57 +0100

    Update settings.py

--- src/ecommestore/settings.py
+++ src/ecommestore/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-ag)-2xlg8@nz7d!xuf&y%=+37&g%i@9$p5z(*g%iwb-vcm@zac'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ["*"]
 
